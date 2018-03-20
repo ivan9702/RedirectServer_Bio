48d89e49121dc89ac7120e1c7fd85896afeaea48
ch <ch@mail.startek-eng.com>
2018-03-20 10:31:33 +0800

    Requiring config and mongoose. Renaming the router from users to redirect. (app.js)

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,8 +7,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var {mongoose} = require('./db/mongoose');
 var index = require('./routes/index');
-var users = require('./routes/users');
+var redirect = require('./routes/redirect');
 
 var app = express();
 
@@ -23,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/redirect', redirect);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
