ba2a48f4b3cc929ebbe5d4832ddf6743d1c76e01
ch <ch@mail.startek-eng.com>
2018-03-21 09:36:21 +0800

    Using the middleware 'serverExists'. Adding a route /enrollFP to send a request to Bioserver with axios to add a FP. Updating the property's name of RedirectData. (redirect.js)

--- routes/redirect.js
+++ routes/redirect.js
@@ -1,6 +1,8 @@
 var express = require('express');
+const axios = require('axios');
 
 const {BioserverId} = require('./../models/bioserverId');
+const serverExists = require('./middleware/serverExists');
 
 var redirect = express.Router();
 
@@ -13,13 +15,13 @@ redirect.post('/addBioserver', (req, res) => {
   if (req.body.bsIP) {
     var newBioserver = new BioserverId({
       bsIP: req.body.bsIP,
-      bsId: RedirectData.topBSId,
+      bsId: RedirectData.lastBSId,
       count: 0,
       updated_at: new Date().toUTCString()
     });
 
     newBioserver.save().then(() => {
-      RedirectData.topBSId++;
+      RedirectData.lastBSId++;
       res.json({code: 200, message: 'New bioserver has been saved.'});
     }, (err) => {
       if (err.code === 11000) {
@@ -27,6 +29,29 @@ redirect.post('/addBioserver', (req, res) => {
       } else {
         res.json({code: 501, message: 'May be Some Error on Input or Server.'});
       }
+    });
+  } else {
+    res.json({code: 406, message: 'Required Columns Not Fullfilled.'});
+  }
+});
+
+redirect.use(serverExists);
+
+redirect.post('/enrollFP', (req, res) => {
+  if (req.body.eSkey && req.body.iv && req.body.encMinutiae && req.body.ClientUserId && req.body.fpIndex) {
+    var addFPURL = RedirectData.AddFPServer + '/api/addFP';
+    // console.log('addFPURL: ' + addFPURL);
+    axios.post(addFPURL, {
+      userId: RedirectData.lastUserId,
+      fpIndex: req.body.fpIndex,
+      eSkey: req.body.eSkey,
+      iv: req.body.iv,
+      encMinutiae: req.body.encMinutiae
+    }).then((response) => {
+      // console.log(response);
+      res.json({code: response.data.code, message: response.data.message});
+    }).catch((error) => {
+      res.json({code: 501, message: 'May be Some Error on Input or Server.'});
     });
   } else {
     res.json({code: 406, message: 'Required Columns Not Fullfilled.'});
